eb0393f46b4ef9fefb9db0eaf7f44be8520fe581
Jasper Pell <little0jasper@gmail.com>
2026-05-29 15:06:32 -0400

    Change spacing of preferences

--- preferences.py
+++ preferences.py
@@ -1,48 +1,48 @@
 # Clip length in seconds
 CLIP_LENGTH = 60
-    # Default: 60
+	# Default: 60
 
 # Output video FPS
 TARGET_FPS = None
-    # Default: None
-        # Keeps the FPS of the original import
-    # Common FPS values:
-        # 24 (Cinematic)
-        # 30 (Standard)
-        # 60 (High Frame Rate)
+	# Default: None
+		# Keeps the FPS of the original import
+	# Common FPS values:
+		# 24 (Cinematic)
+		# 30 (Standard)
+		# 60 (High Frame Rate)
 
 # Export location for clips
 EXPORT_LOCATION = r"Exports"
-    # Default: "Exports" in current directory
+	# Default: "Exports" in current directory
 
 # Encoding method
 ENCODER = '1'
-    # [1] High quality CPU encoding
-    # [2] Fast GPU encoding
+	# [1] High quality CPU encoding
+	# [2] Fast GPU encoding
 # GPU brand (If using GPU encoding)
 GPU_BRAND = '1'
-    # [1] NVIDIA
-    # [2] Intel
-    # [3] AMD
+	# [1] NVIDIA
+	# [2] Intel
+	# [3] AMD
 
 # Crop ratio
 CROP_RATIO = '9:16'
-    # Common aspect ratios:
-        # 1:2 (Vertical)
-        # 9:16 (Vertical)
-        # 2:3 (Vertical)
-        # 5:7 (Vertical)
-        # 3:4 (Vertical)
-        # 4:5 (Vertical)
-        # 1:1 (Square)
-        # 5:4 (Horizontal)
-        # 4:3 (Horizontal)
-        # 7:5 (Horizontal)
-        # 3:2 (Horizontal)
-        # 16:9 (Horizontal)
-        # 2.39:1 (Cinematic Horizontal)
+	# Common aspect ratios:
+		# 1:2 (Vertical)
+		# 9:16 (Vertical)
+		# 2:3 (Vertical)
+		# 5:7 (Vertical)
+		# 3:4 (Vertical)
+		# 4:5 (Vertical)
+		# 1:1 (Square)
+		# 5:4 (Horizontal)
+		# 4:3 (Horizontal)
+		# 7:5 (Horizontal)
+		# 3:2 (Horizontal)
+		# 16:9 (Horizontal)
+		# 2.39:1 (Cinematic Horizontal)
 
 # Show stats during processing
 SHOW_STATS = False
-    # [True] Advanced information will be shown during processing
-    # [False] Only essential information will be shown during processing
+	# [True] Advanced information will be shown during processing
+	# [False] Only essential information will be shown during processing
